4489dc880276e2f2456df0c4cf32a1e472592539
Esther-Ifie <estherifie13@gmail.com>
2026-07-27 12:52:55 +0100

    Delete javaproject directory

--- javaproject/banking-api/src/main/java/net/banking_api/banking/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package net.banking_api.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.banking_api.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    
-}
--- javaproject/banking-api/src/main/java/net/banking_api/banking/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.banking_api.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.banking_api.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    
-}
